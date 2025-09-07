e66c80b518f98a9b46fc91bb667cb93cc10b11b2
NitinBhatia007 <nitinbhatia499@gmail.com>
2025-09-07 13:28:03 +0530

    Hello World program

---
+++ src/ex_001_Java_basic/Lab001_Hello_World.java
@@ -0,0 +1,9 @@
+package ex_001_Java_basic;
+
+import java.util.Arrays;
+
+public class Lab001_Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
